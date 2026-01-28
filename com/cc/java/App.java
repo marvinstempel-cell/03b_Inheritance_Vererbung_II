22993d911bdec7ff2d41bcd22f41515db51336e4
Peter <schmidt.multimedia@gmail.com>
2026-01-28 11:16:42 +0100

    01 / Cat() / TomCat()

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,7 +4,7 @@ public class App {
     
     public static void main(String[] args) {
 
-        Cat cat = new Cat("Grizabella", "white", 29, true);
+        Cat cat = new Cat("Grizabella", "white", 29);
 
         output(cat.getStringAttributes("#name"));
         output(cat.getStringAttributes("#color"));
@@ -16,13 +16,13 @@ public class App {
 
         output("-------------------------");
 
-        Cat cat2 = new Cat("Alonzo", "grey", 35, false);
+        Tomcat tomcat = new Tomcat("Alonzo", "grey", 35);
 
-        output(cat2.getStringAttributes("#name"));
-        output(cat2.getStringAttributes("#color"));
+        output(tomcat.getStringAttributes("#name"));
+        output(tomcat.getStringAttributes("#color"));
 
-        output(cat2.getAge()); 
-        output(cat2.getAge()); 
+        output(tomcat.getAge()); 
+        output(tomcat.getAge()); 
 
 
     }
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,15 +5,13 @@ public class Cat {
   private String firstName;
   private String furColor;
   private int age;
-  private boolean isFemale; 
 
   private int counter;
 
-  public Cat(String firstName, String furColor, int age, boolean isFemale) {
+  public Cat(String firstName, String furColor, int age) {
     this.firstName = firstName;
     this.furColor = furColor;
     this.age = age;
-    this.isFemale = isFemale;
   }
 
   public String getStringAttributes(String op) {
@@ -28,11 +26,7 @@ public class Cat {
   }
 
   public String getAge() {
-    if (isFemale) {
       return checkCompliance();
-    } else {
-      return String.valueOf(age);
-    }
   }
 
 private String checkCompliance(){
